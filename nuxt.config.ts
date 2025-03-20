c96a8d597e0bb382826112310596e09c2278c87b
doobie-droid <lesliedouglas23@gmail.com>
2025-03-20 07:21:10 +0100

    ci: github action workflow created

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  app: {
+    baseURL: "/portfolio/",
+    head: {
+      title: "Portfolio",
+      meta: [
+        {
+          name: "description",
+          content:
+            "This is a static website of the online portfolio of Douglas Leslie, containing links to open source contributions, personal projects, work history, tutorials and other tech related doings",
+        },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ["/"],
+    },
+  },
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+});
