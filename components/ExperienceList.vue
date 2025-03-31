c2e74e7f777427908f41ac2678c5daf8b831bd00
doobie-droid <lesliedouglas23@gmail.com>
2025-03-31 15:04:11 +0100

    feat: update the about page

---
+++ components/ExperienceList.vue
@@ -0,0 +1,23 @@
+<template>
+    <ol class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-24 md:gap-8 md:text-left pt-11">
+        <ExperienceItem v-for="(job, index) in jobs" :key="index" :company="job.company" :role="job.role"
+            :duration="job.duration" />
+    </ol>
+</template>
+
+<script>
+import ExperienceItem from './ExperienceItem.vue'
+
+export default {
+    name: 'ExperienceList',
+    components: {
+        ExperienceItem,
+    },
+    props: {
+        jobs: {
+            type: Array,
+            required: true,
+        },
+    },
+}
+</script>
