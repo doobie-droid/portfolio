2d0cdab3716f52becf61accece1db146453006d7
doobie-droid <lesliedouglas23@gmail.com>
2025-04-10 01:39:00 +0100

    chore: cache busting for redeployment

--- components/ui/LoadingDots.vue
+++ components/ui/LoadingDots.vue
@@ -1,6 +1,6 @@
 <template>
     <div class='flex space-x-2 justify-center items-center  h-10 '>
-        <span class='sr-only'>Loading...</span>
+        <span class='sr-only'>Loading..</span>
         <div class='h-4 w-4 bg-primary dark:bg-yellow-400 rounded-full animate-bounce [animation-delay:-0.3s]'></div>
         <div class='h-4 w-4 bg-primary dark:bg-yellow-400 rounded-full animate-bounce [animation-delay:-0.15s]'></div>
         <div class='h-4 w-4 bg-primary dark:bg-yellow-400 rounded-full animate-bounce'></div>
